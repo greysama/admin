bd31ad55952d1bbc48b613ad8c84d308fe72b92b
greysama <greysama86@gmail.com>
2015-12-23 17:35:33 -0300

    Update dummy.blade.php

--- src/views/dummy.blade.php
+++ src/views/dummy.blade.php
@@ -1,8 +1,8 @@
 <div class="row">
 	<div class="col-lg-12">
-		<h1 class="page-header">Bienvenido</h1>
+		<h1 class="page-header">Bienvenid@!</h1>
 		<div class="alert alert-info">
-			Para cargar contenidos, clickea sobre el menu sobre el tipo de contenido que desees.
+			Para cargar contenidos, clickeá sobre el menu sobre el tipo de contenido que desees.
 		</div>
 	</div>
 </div>
